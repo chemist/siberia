afd83bee0002258cc40bb9cf9bc2ea3605ebc526
chemist <chemistmail@gmail.com>
2013-02-26 17:14:44 +0400

    clean code

--- Main.hs
+++ Main.hs
@@ -4,52 +4,38 @@
 {-# LANGUAGE FlexibleInstances #-}
 module Main where
 
-import Prelude (not, (++), String, Bool(..), (<), lookup, (-), Bool, show, snd, undefined, id, Show, otherwise, Int, ($), (>), (+), fst, (.), (*), (==), (/=), fromIntegral)
-import Control.Monad
+import Prelude ()
+import BasicPrelude
+
 import Control.Monad.State
-import Control.Monad.IO.Class
 import Control.Monad.Trans.Resource
-import Control.Applicative ((<$>), liftA, (<*>))
 import Control.Concurrent hiding (yield)
 import Control.Concurrent.STM
 import Control.Concurrent.STM.TBMChan
-import System.IO (FilePath, hClose, print, IO, openBinaryFile, hClose, IOMode(..))
-import Control.Exception (bracket)
 
 import Data.Conduit
 import Data.Conduit.Network
 import Data.Conduit.TMChan
 import Data.Conduit.List hiding (head, take, drop)
 import qualified Data.Conduit.List as CL
-import Data.Conduit.Binary (sourceLbs, head, take, takeWhile, lines)
 import qualified Data.Conduit.Binary as CB
 import Data.Conduit.Util 
 import qualified Network.HTTP.Conduit as HTTP
 
-import Data.ByteString (breakSubstring, length, split, dropWhile, spanEnd, ByteString, putStr, concat, last, init, null, breakByte, drop)
+import Data.ByteString (breakSubstring, split, spanEnd, breakByte)
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as LB
-import Data.Maybe
-import Data.Word
-import Data.Map (Map)
-import qualified Data.Map as Map
-import Debug.Trace
 import qualified Data.ByteString.Char8 as C
+
+import Data.Maybe (fromJust)
+import qualified Data.Map as Map
 
 type Buffer = TBMChan ByteString
 type Channel = TMChan ByteString
 type Clients = TMVar Int
 
-type BC = (Buffer, Channel, Clients)
-
-type Header = (ByteString, ByteString)
-
-data RadioStream = Meta ByteString
-                 | Headers [Header]
-                 | Stream ByteString 
-                 deriving (Show)
-                 
-                 
+type Headers = [(ByteString, ByteString)]
+type Meta = ByteString
 type Radio = ByteString
 type RadioUrl = ByteString
                  
@@ -60,8 +46,8 @@ data ChannelInfo = ChannelInfo { radio::Radio
                                , clientPid:: Maybe ThreadId
                                , clients:: Int 
                                , connection :: Bool
-                               , headers::Maybe RadioStream
-                               , meta::Maybe RadioStream
+                               , headers::Headers
+                               , meta::Maybe Meta
                                }
 
 type RadioState = MVar (Map Radio (MVar ChannelInfo))
@@ -73,7 +59,6 @@ data Server = Server { port::Port
                      }
 
 type ServerState = StateT Server IO
-
 
 initServer::IO Server
 initServer = do
@@ -93,7 +78,7 @@ addRadio radio url = do
     buffer <- liftIO $ newTBMChanIO 1000
     channel <- liftIO $ buff buffer
     Server{..} <- get
-    info <- liftIO $ newMVar $ ChannelInfo radio url buffer channel Nothing 0 False Nothing Nothing
+    info <- liftIO $ newMVar $ ChannelInfo radio url buffer channel Nothing 0 False [] Nothing
     let fun stMap = return $ Map.insert radio info stMap 
     liftIO $ modifyMVar_ radioState fun
      
@@ -115,13 +100,13 @@ copy buffer chan = forever $ do
     
 server::(Monad m, MonadResource m) => Server -> Application m
 server st ad = do
-    ((request,_):headers) <- appSource ad $$ parseHeaders id
-    let radio = parseRequest request
-    liftIO $ print "request"
+    ((req,_):headers) <- appSource ad $$ parseHeaders id
+    let radio = parseRequest req
+    liftIO $ print ("request"::String)
     liftIO $ print radio
     isChannel <-  liftIO $ haveChannel radio $ radioState st
     connection <-  liftIO $ haveConnection isChannel radio $ radioState st
-    liftIO $ print "headers"
+    liftIO $ print ("headers"::String)
     liftIO $ print headers
     liftIO $ print $ "is channel " ++ show isChannel ++ " is first " ++ show connection
     case (isChannel, connection) of
@@ -139,10 +124,10 @@ server st ad = do
 startClientForServer :: MVar ChannelInfo ->  ResourceT IO ()
 startClientForServer channelMVar = do
     url' <- liftIO $ url <$> readMVar channelMVar
-    request <- HTTP.parseUrl $ C.unpack url'
-    let port = HTTP.port request
-        host = HTTP.host request
-        path = HTTP.path request
+    req <- HTTP.parseUrl $ C.unpack url'
+    let port = HTTP.port req
+        host = HTTP.host req
+        path = HTTP.path req
         settings = clientSettings port host
 -- | стартуем клиента до этого радио
     runTCPClient settings $ clientForServer channelMVar path
@@ -150,30 +135,30 @@ startClientForServer channelMVar = do
 
 clientForServer :: MVar ChannelInfo -> ByteString -> Application (ResourceT IO)
 clientForServer mv path ad = do
-    liftIO $ print "start client for server"
-    let request = BS.concat ["GET ", path, " HTTP/1.1\r\nIcy-MetaData: 1\r\n\r\n"]
-    sourceList [request] $$ appSink ad
-    (l,((state,_):headers)) <- appSource ad $$  zipSinks getLength $ parseHeaders id
-    liftIO $ print "client"
+    liftIO $ print ("start client for server"::String)
+    let req = BS.concat ["GET ", path, " HTTP/1.1\r\nIcy-MetaData: 1\r\n\r\n"]
+    sourceList [req] $$ appSink ad
+    (l, (st,_):headers) <- appSource ad $$  zipSinks getLength $ parseHeaders id
+    liftIO $ print ("client"::String)
     liftIO $ print l
-    liftIO $ print state
+    liftIO $ print st
     liftIO $ print headers
     let metaInt = fromMaybe 0 $ do m <- lookup "icy-metaint" headers
                                    (i, _) <- C.readInt m
                                    return i
-    meta' <- appSource ad $$ parseMeta metaInt l
-    liftIO $ print meta'
+    meta'' <- appSource ad $$ parseMeta metaInt l
+    liftIO $ print meta''
     buffer' <- liftIO $ getBuffer mv
     liftIO $ setConnection mv
-    liftIO $ print "make pipe server => buffer"
+    liftIO $ print ("make pipe server => buffer"::String)
     appSource ad $$ sinkTBMChan buffer'
     
 
 ------------------------ client only --------------------------------------------------
  
 startClient::(Monad m, MonadIO m) => MVar ChannelInfo -> Source m ByteString -> Sink ByteString m () -> m ()
-startClient channelMVar sourceI sinkI = do
-    let response = concat [ "ICY 200 OK\r\n"
+startClient channelMVar _ sinkI = do
+    let resp = concat [ "ICY 200 OK\r\n"
                           , "icy-notice1: Haskell shoucast splitter\r\n"
                           , "icy-notice2: Good music here\r\n"
                           , "content-type:audio/mp3\r\n"
@@ -181,7 +166,7 @@ startClient channelMVar sourceI sinkI = do
                           , "icy-metaint:8192\r\n"
                           , "icy-br:64\r\n\r\n"
                           ]
-    sourceList [response] $$ sinkI
+    sourceList [resp] $$ sinkI
     channel <- liftIO $ channel <$> readMVar channelMVar
     dup <- liftIO $ atomically $ dupTMChan channel
     sourceTMChan dup $$ sinkI
@@ -195,7 +180,7 @@ haveChannel radio radioState = withMVar radioState (fun radio)
 -- | есть соединение до радиостанции?
 haveConnection::Bool -> Radio -> RadioState -> IO Bool
 haveConnection isChannel radio' radioState'
-              | isChannel == False = return False
+              | not isChannel = return False
               | otherwise = do
    ChannelInfo{..} <- fromJust <$> getChannel radio' radioState'
    return connection
@@ -227,19 +212,21 @@ parseMeta::(Monad m, MonadIO m) => Int -> Int -> Sink ByteString m LB.ByteString
 parseMeta metaInt headerLength = do
     CB.drop (metaInt - headerLength)
     len <- CB.head 
-    let len' = 16 * (fromIntegral $ fromJust len)
+    let toInt = fromIntegral . fromJust
+        len' = 16 * toInt len
     CB.take len'
     
 -- | размер после запроса
 getLength::(Monad m, MonadIO m, MonadResource m) => Sink ByteString m Int
-getLength = sinkState 0 (\state input -> do
+getLength = sinkState 0 (\st input -> do
     let (_,b) = breakSubstring "\r\n\r\n" input
-    case (null b, state < 3) of
-        (True, True) -> return $ StateProcessing $ state + 1 
+    case (BS.null b, st < 3) of
+        (True, True) -> return $ StateProcessing $ st + 1 
         (True, False) -> return $ StateDone Nothing 0  
-        (False, _) -> return $ StateDone Nothing $ length b -4)
+        (False, _) -> return $ StateDone Nothing $ BS.length b -4)
                         return
 
+charLF, charCR, charSpace, charColon :: Word8
 charLF = 10
 charCR = 13
 charSpace = 32
@@ -253,27 +240,27 @@ sinkLine = do
     
 killCR::ByteString -> ByteString
 killCR bs 
-   | null bs = bs
-   | last bs == charCR = init bs
+   | BS.null bs = bs
+   | BS.last bs == charCR = BS.init bs
    | otherwise = bs
    
 parseRequest::ByteString -> Radio
-parseRequest line = let (r:url:h) = split charSpace line
+parseRequest line = let (_:url:_) = split charSpace line
                     in url
  
 parseHeader::Monad m => ByteString -> Sink ByteString m (ByteString, ByteString)
 parseHeader bs = do
     let (key, bs2) = breakByte charColon bs
-    return (strip key, strip $ drop 1 bs2)
+    return (strip key, strip $ BS.drop 1 bs2)
 
 strip::ByteString -> ByteString
-strip = dropWhile (== charSpace) . fst . spanEnd (== charSpace)
+strip = BS.dropWhile (== charSpace) . fst . spanEnd (== charSpace)
 
     
-parseHeaders::(Monad m, MonadIO m, MonadResource m) => ([Header] -> [Header]) -> Sink ByteString m [Header]
+parseHeaders::(Monad m, MonadIO m, MonadResource m) => (Headers -> Headers) -> Sink ByteString m Headers
 parseHeaders front = do
     line <- sinkLine
-    if null line
+    if BS.null line
        then return $ front []
        else do
            header <- parseHeader line
@@ -281,12 +268,9 @@ parseHeaders front = do
   
   
 
-request::LB.ByteString
+request, response, meta' :: LB.ByteString
 request = LB.fromChunks ["GET / HTTP/1.0\r\nUser-Agent: mpg123/1.14.4\r\nHost: radio.bigblueswing.com:8002\r\nAccept: audio/mpeg, audio/x-mpeg, audio/mp3, audio/x-mp3, audio/mpeg3, audio/x-mpeg3, audio/mpg, audio/x-mpg, audio/x-mpegaudio, application/octet-stream, audio/mpegurl, audio/mpeg-url, audio/x-mpegurl, audio/x-scpls, audio/scpls, application/pls, */*\r\nIcy-MetaData: 1\r\n\r\n"]
-
-response::LB.ByteString
 response = LB.fromChunks ["ICY 200 OK\r\nicy-notice1:<BR>This stream requires <a href=\"http://www.winamp.com/\">Winamp</a><BR>\r\nicy-notice2:SHOUTcast Distributed Network Audio Server/Linux v1.9.8<BR>\r\n"]
-
 meta' = LB.fromChunks ["ncontent-type:audio/mpeg\r\nicy-pub:1\r\nicy-metaint:8192\r\nicy-br:64\r\n\r\n"]
     
 -- http://radio.bigblueswing.com:8002/
