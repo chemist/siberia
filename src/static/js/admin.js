4ccd3f72c37a16d07323264a4b61c9571c028439
outself <outself@outself.ru>
2013-03-10 18:10:27 +0400

    crud js logic

--- src/static/js/admin.js
+++ src/static/js/admin.js
@@ -1,13 +1,34 @@
 var admin = angular.module('admin', ['ngResource', 'ngCookies']);
 
+admin.config(function($routeProvider, $locationProvider){
+    $routeProvider.when('/', {redirectTo: '/streams'});
+    $routeProvider.when('/status', {controller: 'StatusCtrl', templateUrl: '/status.html'});
+    $routeProvider.when('/streams', {controller: 'ListCtrl', templateUrl: '/streams.html'});
+    $routeProvider.when('/streams/add', {controller: 'AddCtrl', templateUrl: '/streams-add.html'});
+});
+
 admin.factory('Stream', function($resource){
     return $resource('/stream/:id', {'id': '@id'});
 });
 
-admin.controller('StreamsCtrl', function($scope, Stream){
+admin.controller('ListCtrl', function($scope, Stream){
     $scope.searchText = '';
+    $scope.streams = Stream.query();
+});
 
-    Stream.query(function(streams){
-        $scope.streams = streams;
-    });
+admin.controller('AddCtrl', function($scope, Stream){
+    $scope.stream = {
+        id: 'ru-ah-fm',
+        url: 'http://ru.ah.fm/'
+    };
+
+    $scope.add = function() {
+        Stream.save($scope.stream, function(){
+            console.log(arguments);
+        });
+    };
+});
+
+admin.controller('StatusCtrl', function($scope){
+
 });
