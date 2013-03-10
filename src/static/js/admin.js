56c7c9b3ee45ddff44466b0744555f46b1bd79b9
outself <outself@outself.ru>
2013-03-10 17:03:34 +0400

    stream list filtering

--- src/static/js/admin.js
+++ src/static/js/admin.js
@@ -1,1 +1,19 @@
 var admin = angular.module('admin', ['ngResource', 'ngCookies']);
+
+admin.factory('Stream', function($resource){
+    return $resource('/stream/:id', {'id': '@id'});
+});
+
+admin.controller('StreamsCtrl', function($scope, Stream){
+    $scope.searchText = '';
+
+    $scope.streams = [
+        {id: 12, url: 'http://ru.ah.fm/'},
+        {id: 13, url: 'http://bassjunkies.com'}
+    ];
+
+    //var streams = Stream.query(function(){
+    //    console.log(streams);
+    //    $scope.streams = streams;
+    //});
+});
